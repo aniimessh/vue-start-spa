9e9b3d190ac0517f572f43ac839706d7d8e05245
aniimessh <animeshgupta2.002@gmail.com>
2024-05-03 19:30:45 +0530

    learning Vuejs

---
+++ src/components/Navbar.vue
@@ -0,0 +1,73 @@
+<template>
+  <nav class="navbar navbar-expand-lg" :class="[`navbar-${theme}`, `bg-${theme}`]">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav d-flex justify-content-between">
+          <li v-for="(page, index) in pages">
+            <navbar-link
+              :page="page"
+              :isActive="activePage == index"
+              @click.prevent="navLinkClick(index)"
+            >
+            </navbar-link>
+          </li>
+          <form action="" class="d-flex">
+            <button class="btn btn-primary" @click.prevent="changeTheme()">Toggle</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import NavbarLink from "./NavbarLink.vue";
+export default {
+  components: {
+    NavbarLink,
+  },
+  created() {
+    this.getThemeSettings();
+  },
+  props: ["pages", "activePage", "navLinkClick"],
+  data() {
+    return {
+      theme: "light",
+    };
+  },
+  methods: {
+    changeTheme() {
+      let theme = "light";
+
+      if (this.theme == "light") {
+        theme = "dark";
+      }
+      this.theme = theme;
+      this.storeThemeSettings();
+    },
+    storeThemeSettings() {
+      localStorage.setItem("theme", this.theme);
+    },
+    getThemeSettings() {
+      let theme = localStorage.getItem("theme");
+      if (theme) {
+        this.theme = theme;
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/PageViewer.vue
@@ -0,0 +1,28 @@
+<template>
+        <div class="container">
+                <h1 class="emphasize">{{ page.title }}</h1>
+                <p>{{ page.content }}</p>
+        </div>
+</template>
+
+<script>
+export default {
+        props: {
+                page: {
+                        type: Object,
+                        default(rawProps){
+                        return {
+                                title: '',
+                                content: ''
+                        }
+                }
+        }
+},
+};
+</script>
+
+<style scoped>
+.emphasize {
+        color: blue;
+}
+</style>
